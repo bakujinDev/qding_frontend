5181fd92c29dda4d7c2961c9b524e9d48befbd8c
bak-u-jin <87050600+bakujinDev@users.noreply.github.com>
2023-02-11 18:30:32 +0900

    create question cont area

--- src/pages/qna/[id]/index.tsx
+++ src/pages/qna/[id]/index.tsx
@@ -2,13 +2,15 @@ import { getQnaPost } from "@/api/qna";
 import Seo from "@/components/Seo";
 import { timeDifference } from "@/lib/time";
 import useUser from "@/lib/user";
-import { setLoginPopup } from "@/store/reducer/commonReducer";
 import { useQuery } from "@tanstack/react-query";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
-import { toast } from "react-toastify";
 import styles from "./qnaPosts.module.scss";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import FlagIcon from "@mui/icons-material/Flag";
+import HistoryIcon from "@mui/icons-material/History";
 
 export default function QnaPosts() {
   const router = useRouter();
@@ -19,8 +21,6 @@ export default function QnaPosts() {
   const { data, isSuccess } = useQuery(["postQuery", `${id}`], getQnaPost, {
     retry: false,
   });
-
-  console.log(isSuccess);
 
   return isSuccess ? (
     <>
@@ -48,12 +48,84 @@ export default function QnaPosts() {
           </article>
 
           <article className={styles.contArea}>
-            <ReactQuill
-              className={`${styles.quill} readOnlyQuill`}
-              readOnly={true}
-              modules={{ toolbar: false }}
-              value={data.content}
-            />
+            <div className={styles.voteBox}>
+              <button
+                className={`${styles.upBtn} ${styles.voteBtn}`}
+                onClick={() => {}}
+              >
+                <ThumbUpAltIcon />
+              </button>
+
+              <p>{data.votes}</p>
+
+              <button
+                className={`${styles.downBtn} ${styles.voteBtn}`}
+                onClick={() => {}}
+              >
+                <ThumbDownAltIcon />
+              </button>
+
+              <button className={styles.historyBtn} onClick={() => {}}>
+                <HistoryIcon />
+              </button>
+
+              {/* <button className={styles.flagBtn} onClick={() => {}}>
+                <FlagIcon />
+              </button> */}
+            </div>
+
+            <div className={styles.contCont}>
+              <div className={styles.quillBox}>
+                <ReactQuill
+                  className={`${styles.quill} readOnlyQuill`}
+                  readOnly={true}
+                  modules={{ toolbar: false }}
+                  value={data.content}
+                />
+              </div>
+
+              <div className={styles.tagBar}>
+                <ul className={styles.tagList}>
+                  {data.tag.map((tag: any, i: number) => (
+                    <li key={i}>{tag.name}</li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className={styles.utilBar}>
+                <div className={styles.btnBox}>
+                  <button className={styles.editBtn} onClick={() => {}}>
+                    수정하기
+                  </button>
+
+                  <button className={styles.followBtn} onClick={() => {}}>
+                    알람받기
+                  </button>
+
+                  <button className={styles.shareBtn} onClick={() => {}}>
+                    공유하기
+                  </button>
+                </div>
+
+                <div className={styles.profBox}>
+                  <img
+                    src={data.editor?.avatar || data.creator.avatar}
+                    alt=""
+                  />
+                  <p className={styles.name}>
+                    {data.editor?.name || data.creator.name}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+
+          <article className={styles.replyArea}>
+            <ul className={styles.replyList}>
+            {/* {.map((v, i) => (
+            <li key={i}>{v}</li>
+            ))} */}
+            </ul>
           </article>
         </section>
 
